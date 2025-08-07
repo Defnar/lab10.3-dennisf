70cf0416ac7ff10bc7327a3e51395ef1bed742a9
Dennis <dennis-farris@hotmail.com>
2025-08-06 23:25:02 -0400

    finished up styling items on the list, removed completed button now disappears if nothing checked, selected filter is now outlined in blue

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,10 @@
-import { useContext, useState } from "react";
+import { useContext, useMemo, useState } from "react";
 import type { Todo } from "../utils/type";
-import { TodoContext } from "../contexts/context";
+import { ThemeContext, TodoContext } from "../contexts/context";
 
 export default function TodoItem({ id, text, completed }: Todo) {
   const { toggleTodo, deleteTodo, editTodo } = useContext(TodoContext);
+  const {theme} = useContext(ThemeContext);
 
   const [textValue, setTextValue] = useState<string>(text);
 
@@ -13,12 +14,10 @@ export default function TodoItem({ id, text, completed }: Todo) {
     setTextValue(e.target.value);
   };
 
-  const handleEdit = (
-    e: React.KeyboardEvent | React.FocusEvent
-  ) => {
+  const handleEdit = (e: React.KeyboardEvent | React.FocusEvent) => {
     if (
-      (e.type==="keydown" && (e as React.KeyboardEvent).key === "Enter") ||
-      e.type==="blur"
+      (e.type === "keydown" && (e as React.KeyboardEvent).key === "Enter") ||
+      e.type === "blur"
     ) {
       const newText = textValue.trim() !== "" ? textValue.trim() : text;
       editTodo(id, newText);
@@ -26,29 +25,38 @@ export default function TodoItem({ id, text, completed }: Todo) {
     }
   };
 
+  const liStyles = useMemo(() => theme==="dark"? "border-slate-600": "border-slate-400" ,[theme])
+  const inputStyles = useMemo(() => theme==="dark"? "bg-slate-600" : "white", [theme])
+
+
   return (
-    <li>
+    <li className={`border-t border-solid ${liStyles} px-2`}>
       {!editing && (
-        <>
-          <input
-            type="checkbox"
-            checked={completed}
-            onChange={() => toggleTodo(id)}
-          />
-          {text}
-          <button type="button" onClick={() => setEditing(true)}>
-            Edit
-          </button>
-          <button type="button" onClick={() => deleteTodo(id)}>
-            Delete
-          </button>
-        </>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row gap-4">
+            <input
+              type="checkbox"
+              checked={completed}
+              onChange={() => toggleTodo(id)}
+            />
+            {text}
+          </div>
+          <div className = "flex flex-row gap-6">
+            <button type="button" onClick={() => setEditing(true)}>
+              Edit
+            </button>
+            <button type="button" onClick={() => deleteTodo(id)}>
+              Delete
+            </button>
+          </div>
+        </div>
       )}
       {editing && (
         <>
           <input
             type="text"
             value={textValue}
+            className={`w-full px-4 rounded-md ${inputStyles}`}
             autoFocus
             onChange={handleInput}
             onBlur={handleEdit}
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,49 +6,42 @@ import type { Filter, Todo } from "../utils/type";
 export default function TodoList() {
   const { todos, clearCompleted } = useContext(TodoContext);
 
-  const {theme} = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
 
   //states to hold filter data
-  const  [filteredList, setFilteredList] = useState<Todo[]>(todos);
+  const [filteredList, setFilteredList] = useState<Todo[]>(todos);
   const [filter, setFilter] = useState<Filter>("All");
-
-
 
   //counts number of active items
   const [activeCount, setActiveCount] = useState<number>(0);
 
-
-
   //commented out for personal learning purposes.  Did research on better ways to count conditional items in a list in react
-//   //updates count of active items on the list
-//   useEffect(() => {
-//     setActiveCount(0);
-//     for (const item of todos) {
-//         if(!item.completed) {
-//             setActiveCount(count => ++count);
-//         }
-//     }
-//   }, [todos])
+  //   //updates count of active items on the list
+  //   useEffect(() => {
+  //     setActiveCount(0);
+  //     for (const item of todos) {
+  //         if(!item.completed) {
+  //             setActiveCount(count => ++count);
+  //         }
+  //     }
+  //   }, [todos])
 
-//version 2 update count of active items, using filter instead
-useEffect(() => {
-    const activeList = todos.filter(item => !item.completed)
-    setActiveCount(activeList.length);
-}, [todos])
-
-
-//updates filter list to display to user
+  //version 2 update count of active items, using filter instead
   useEffect(() => {
-    const filterList = todos.filter(item => {
-        if (filter==="All") return item
-        else if (filter ==="Active") {
-            return !item.completed
-        }
-        else return item.completed
-    })
-    setFilteredList(filterList);
-  }, [filter, todos])
+    const activeList = todos.filter((item) => !item.completed);
+    setActiveCount(activeList.length);
+  }, [todos]);
 
+  //updates filter list to display to user
+  useEffect(() => {
+    const filterList = todos.filter((item) => {
+      if (filter === "All") return item;
+      else if (filter === "Active") {
+        return !item.completed;
+      } else return item.completed;
+    });
+    setFilteredList(filterList);
+  }, [filter, todos]);
 
   //build list using filtered list and todo item component
   const listBuilder = () => {
@@ -62,19 +55,60 @@ useEffect(() => {
     ));
   };
 
-  const buttonStyles = useMemo(() => `w-30 rounded-md py-2 px-4 ${theme==="dark"? "bg-slate-600" : "bg-slate-800"}` ,[theme])
+  const buttonStyles = useMemo(
+    () =>
+      `w-30 rounded-md py-2 px-4 ${
+        theme === "dark" ? "bg-slate-600" : "bg-slate-800 text-white"
+      }`,
+    [theme]
+  );
+
+  const activeButton = useMemo(() => {
+    return (button: Filter) => filter===button? "ring-2 ring-blue-400" : ""
+  }, [filter])
 
   return (
     <>
-    <div className="flex flex-row justify-center gap-4 align-content-center">
-        <button type="button" value="All" className={`${buttonStyles}`} onClick={() => setFilter("All")}>All</button>
-        <button type="button" value="Active" className={`${buttonStyles}`} onClick={() => setFilter("Active")}>Active</button>
-        <button type="button" value="Completed" className={`${buttonStyles}`} onClick={() => setFilter("Completed")}>Completed</button>
-    </div>
-      <ul className="list-none leading-11 h-40 overflow-y-scroll">{listBuilder()}</ul>
-      
-      <p>{activeCount} item{activeCount!=1? "s" : ""} left</p>
-      <button type="button" onClick={clearCompleted}>clear completed</button>
+      <div className="flex flex-row justify-center gap-4 align-content-center">
+        <button
+          type="button"
+          value="All"
+          className={`${buttonStyles} ${activeButton("All")}`}
+          onClick={() => setFilter("All")}
+        >
+          All
+        </button>
+        <button
+          type="button"
+          value="Active"
+          className={`${buttonStyles} ${activeButton("Active")}`}
+          onClick={() => setFilter("Active")}
+        >
+          Active
+        </button>
+        <button
+          type="button"
+          value="Completed"
+          className={`${buttonStyles} ${activeButton("Completed")}`}
+          onClick={() => setFilter("Completed")}
+        >
+          Completed
+        </button>
+      </div>
+      <ul className="list-none leading-11 h-40 overflow-y-scroll">
+        {listBuilder()}
+      </ul>
+
+      <div className="flex flex-row justify-between">
+        <p>
+          {activeCount} item{activeCount != 1 ? "s" : ""} left
+        </p>
+        {todos.length !== activeCount &&
+        <button type="button" className={buttonStyles} onClick={clearCompleted}>
+          clear completed
+        </button>
+}
+      </div>
     </>
   );
 }
