588c9c73d5f8a113cfa3c813b5875a16e6b52d1c
Dennis <dennis-farris@hotmail.com>
2025-08-06 22:37:20 -0400

    colors added based on theme, box sizing and rounded done, some header component styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { useContext, useMemo } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+import { ThemeContext } from "./contexts/context";
 
 function App() {
+  const { theme } = useContext(ThemeContext);
+
+  const styles = useMemo(() => theme === "dark" ? "bg-slate-800 text-white" : "bg-slate-200 text-black", [theme]);
+
+
   return (
-  <>
-  <Header />
-  <TodoInput />
-  <TodoList />;
-  </>
-  )
+    <div
+      className={`flex flex-col content-center justify-center w-150 px-20 py-10 rounded-lg ${styles}`}
+    >
+      <Header />
+      <TodoInput />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
-import { useContext } from "react"
-import { ThemeContext } from "../contexts/context"
+import { useContext, useMemo } from "react";
+import { ThemeContext } from "../contexts/context";
 
 export default function Header() {
+  const {theme, toggleTheme } = useContext(ThemeContext);
 
-    const {toggleTheme} = useContext(ThemeContext);
+const buttonStyles = useMemo(() =>  theme==="dark"? "border-slate-200 bg-slate-600": "border-slate-800 bg-slate-200",
+[theme] )
 
-
-
-    return(
-        <>
-        <h1>Dennis' Todo App</h1>
-        <button type="button" onClick={toggleTheme}>Dark Mode</button>
-        </>
-    )
+  return (
+    <div className="relative flex items-center">
+      <h1 className="absolute left-1/2 transform -translate-x-1/2 font-bold text-2xl">Dennis' Todo App</h1>
+      <button type="button" className={`ml-auto mr-4 px-3 py-1 border ${buttonStyles}`} onClick={toggleTheme}>
+        Dark Mode
+      </button>
+    </div>
+  );
 }
